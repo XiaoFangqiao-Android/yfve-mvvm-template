07ddaf8e51e3e77765978994de3970c8913a572a
GitHub Action <action@github.com>
2021-06-07 07:14:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xiaofangqiaoandroid/yfvemvvmtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xiaofangqiaoandroid.yfvemvvmtemplate.services
+
+import com.github.xiaofangqiaoandroid.yfvemvvmtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
